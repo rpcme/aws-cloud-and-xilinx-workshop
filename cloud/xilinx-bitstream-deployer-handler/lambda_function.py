885a1254cd2cf344db83e3787bd93e46b83d81ad
Xilinx User <yunq@xilinx.com>
2018-11-16 22:55:00 +0000

    update all the lambda functions

--- cloud/xilinx-bitstream-deployer-handler/lambda_function.py
+++ cloud/xilinx-bitstream-deployer-handler/lambda_function.py
@@ -1,43 +1,74 @@
 import logging
 import sys
-import time
+import os
+import subprocess
 import json
 import boto3
 from threading import Timer
-
 import greengrasssdk
 from botocore.session import Session
 
-# Setup logging to stdout
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
 
 client = greengrasssdk.client('iot-data')
 s3 = boto3.resource('s3')
 
-# This is where the overlay will output files.
-path = "/home/xilinx/packages"
-bucket = "bitstream-deployer"
+
+# This is where the bitstream is stored
+# Assume there is only 1 folder with suffix `-s3` in "/home/xilinx"
+bucket = glob.glob1('/home/xilinx', '*-s3')[0]
+sync_folder_path = os.path.join("/home/xilinx", bucket)
 topic = "/generator/camera"
+bitstream = 'gstreamer_deephi.bit'
+parameters = 'parameters.txt'
 
-client = greengrasssdk.client('iot-data')
 
-def copy_to_s3(file):
+def download_from_cloud():
     session = Session()
-    creds = session.get_credentials()
-    s3.meta.client.upload_file('{}/{}'.format(path,file), bucket, 'web/camera-images/{}'.format(file))
+    _ = session.get_credentials()
+
+    # download bitstream
+    s3.meta.client.download_file(bucket, bitstream,
+                                 os.path.join(sync_folder_path, bitstream))
+    client.publish(topic=topic,
+                   payload='Downloaded {} from S3.'.format(bitstream))
+
+    # download parameters if exists
+    try:
+        s3.Object(bucket, 'parameters.txt').load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            pass
+        else:
+            raise
+    else:
+        s3.meta.client.download_file(bucket, parameter,
+                                     os.path.join(sync_folder_path, parameters))
+        client.publish(topic=topic,
+                       payload='Downloaded {} from S3.'.format(parameters))
+
     return
 
-def inference_watcher():
-    Timer(1, inference_watcher).start()
 
-inference_watcher()
+def start_video_inference():
+    num_seconds = 5
+    threshold = 2
+    # video application parameters are read from file if possible
+    if os.path.isfile(os.path.join(sync_folder_path, parameters)):
+        with open(os.path.join(sync_folder_path, parameters)) as f:
+            num_seconds = f.read()
+            threshold = f.read()
+    _ = subprocess.check_output(
+        'cd {0} & /usr/local/bin/pydeephi_yolo.py {1} {2}'.format(
+            sync_folder_path, num_seconds, threshold), shell=True)
+
+
+download_from_cloud()
+start_video_inference()
+
 
 def lambda_handler(event, context):
-    logger.info("Received event data: {}".format(json.dumps(event)))
-    client.publish(topic='hello/world', payload='Sent from Greengrass Core.')
-    if os.path.isfile(volumePath + '/log.txt'):
-        with open(volumePath + '/log.txt', 'r') as myfile:
-            data = myfile.read()
-        client.publish(topic='hello/world', payload=data)
     return
