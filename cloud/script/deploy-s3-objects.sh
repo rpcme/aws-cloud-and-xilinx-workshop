7090aebce69ead97680eee531cb341ede2f28076
Richard Elberger <elberger@amazon.com>
2018-11-24 05:45:05 -0500

    check that the script is being run from the Ultra96, and copy bitstream files to versioned target location

--- cloud/script/deploy-s3-objects.sh
+++ cloud/script/deploy-s3-objects.sh
@@ -6,15 +6,30 @@ if test -z "${prefix}"; then
   exit 1
 fi
 
-bitstream=/usr/local/lib/python3.6/dist-packages/pydeephi/boards/Ultra96/gstreamer_deephi.bit
+f_bitstream=gstreamer_deephi.bit
+d_bitstream=/usr/local/lib/python3.6/dist-packages/pydeephi/boards/Ultra96
+bitstream=${d_bitstream}/${f_bitstream}
+
+d_parameters=/tmp
+f_parameters=parameters.txt
+parameters=${d_parameters}/${f_parameters}
+
 bucket_name=${prefix}-aws-cloud-and-xilinx-workshop
 local_path=/home/xilinx/${bucket_name}
 bucket_policy_location=./bucket-policy.json
+
+if test ! -f ${bitstream}; then
+  echo ERROR: Bitstream file not found:
+  echo "       ${bitstream}"
+  echo "       This file is required for Lab 4.  Ensure you are issuing this"
+  echo "       command from the Ultra96 development board."
+  exit 1
+fi
+
 bucket=$(aws s3api create-bucket --output text \
              --create-bucket-configuration '{ "LocationConstraint": "us-west-2" }' \
              --bucket "${bucket_name}" \
-             --query Location \
-             --create-bucket-configuration LocationConstraint=eu-west-1)
+             --query Location)
 
 my_ip=$(curl ifconfig.co)
 
@@ -43,13 +58,26 @@ echo Constraining bucket access to this specific device
 
 aws s3api put-bucket-policy --bucket ${bucket_name} --policy file://${bucket_policy_location}
 
-
 echo Upload files to S3 bucket
 mkdir -p ${local_path}
 cp -f ${bitstream} ${local_path}
-cat <<EOF > ${local_path}/parameters.txt
+cat <<EOF > ${parameters}
 5
 2
 EOF
-aws s3 sync ${local_path} s3://${bucket_name}/ --acl public-read
+
+# Right now the same bitstream file is being used. This shouldn't be the case.
+# When separate bitstreams are used, the source to copy will be different
+# between the versions.
+
+for i in 1 2; do
+  aws s3 cp ${bitstream}  s3://${bucket_name}/bitstream_deploy/${i}/${f_bitstream}
+  aws s3 cp ${parameters} s3://${bucket_name}/bitstream_deploy/${i}/${f_paramters}
+done
+
+#aws s3 sync ${local_path} s3://${bucket_name}/ --acl public-read
+
+# When the bitstream download operation takes place during Greengrass lambda
+# invocation, the birstream will be placed here.
+
 mkdir -p /home/xilinx/download
